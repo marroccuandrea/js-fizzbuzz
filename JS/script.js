48ca7c99fd62b7d30da8c6fb10fcea442fd3b29e
marroccuandrea <marroccuandrea@yahoo.it>
2024-02-22 16:18:29 +0100

    Stampa in html quadrati con Fizz, Buzz & FizzBuzz

--- JS/script.js
+++ JS/script.js
@@ -2,17 +2,17 @@ const contQ = document.getElementById('cont-q');
 for(let i = 1; i <= 100; i++){
     if((i % 3 === 0) && (i % 5 === 0)){
         contQ.innerHTML += `
-        <div class="box box-5-3">${i}</div>
+        <div class="box box-5-3">FizzBuzz</div>
         `
     }
     else if(i % 5 === 0){
         contQ.innerHTML += `
-        <div class="box box-5">${i}</div>
+        <div class="box box-5">Buzz</div>
         `
     }
     else if(i % 3 === 0){
         contQ.innerHTML += `
-        <div class="box box-3">${i}</div>
+        <div class="box box-3">Fizz</div>
         `
     }
     else{
