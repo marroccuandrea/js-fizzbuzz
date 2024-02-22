3c92b9a5d02389ebca7bd4e6c0011936e5236965
marroccuandrea <marroccuandrea@yahoo.it>
2024-02-22 16:02:01 +0100

    [add] stampo in console i risultati

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+const contQ = document.getElementById('cont-q');
+for(let i = 1; i <= 100; i++){
+    if((i % 3 === 0) && (i % 5 === 0)){
+        // console.log(i);
+    }
+    else if(i % 5 === 0){
+        // console.log(i);
+    }
+    else if(i % 3 === 0){
+        // console.log(i);
+    }
+    else{
+        
+    }
+    console.log(i);
+} 
